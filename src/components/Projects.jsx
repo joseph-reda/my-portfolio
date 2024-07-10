23f755a6374f0972463d87e6fb6a272ed3106b8e
Joseph Reda Rizk <143938394+joseph-reda@users.noreply.github.com>
2024-07-10 06:45:45 +0300

    update latest project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,7 @@ import "./Projects.css";
 import React, { useContext } from "react";
 import MyContext from "./MyContext";
 
-import img1 from "../images/social.png";
+import img1 from "../images/download.png";
 
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
@@ -18,21 +18,19 @@ export const Projects = () => {
         <div className="row d-flex gap-5">
           <div className="card col-lg-4 align-items-center">
             <img src={img1} alt="" />
-            <span>2023</span>
-            <h4>Social media website</h4>
+            <span>2024</span>
+            <h4>Youtube Clone</h4>
           </div>
 
           <div className="card col-lg-7 col-md-8 col-sm-12  d-flex align-items-center">
             <span>Description</span>
             <p>
-              It is a mini application for social networking applications. It
-              includes creating a new account and registering, as well as
-              publishing posts and placing a commint, because I use it on this
-              website, API Real, and also the axios library and tailwindcss.
+              By using react, real_api and tailwindcss I have created a
+              youtube-clone
             </p>
             <div className="d-flex w-100 justify-content-between">
               <a
-                href="https://github.com/joseph-reda/social-media"
+                href="https://github.com/joseph-reda/Youtube-Clone"
                 className="d-flex gap-2 align-items-center ms-5"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -40,15 +38,15 @@ export const Projects = () => {
                 <FontAwesomeIcon icon={faGithub} />
                 <span>Github</span>
               </a>
-                <a
-                  href="https://social-media-dun-eight.vercel.app/"
-                  className="d-flex gap-2 align-items-center me-5"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <span>Vist</span>
-                  <FontAwesomeIcon icon={faArrowRight} />
-                </a>
+              <a
+                href="https://youtube-clone-amber-kappa.vercel.app/"
+                className="d-flex gap-2 align-items-center me-5"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span>Vist</span>
+                <FontAwesomeIcon icon={faArrowRight} />
+              </a>
             </div>
           </div>
         </div>
